024a9ab7e9a7a58fa16d13c1802a0417cce6d892
UsernameZlatan <112383049+UsernameZlatan@users.noreply.github.com>
2022-08-29 11:01:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 
-🥣 🥗 🐔 🥑🍞
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
